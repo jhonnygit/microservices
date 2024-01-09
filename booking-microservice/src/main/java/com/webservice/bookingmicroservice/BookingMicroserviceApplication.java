28687768e27be3a231de2b88301db802738aeda5
jhonnygit <jhonprograma@gmail.com>
2024-01-08 20:35:17 -0400

    discovery eureka

--- booking-microservice/src/main/java/com/webservice/bookingmicroservice/BookingMicroserviceApplication.java
+++ booking-microservice/src/main/java/com/webservice/bookingmicroservice/BookingMicroserviceApplication.java
@@ -1,4 +1,4 @@
-package com.webservice.productmicroservice;
+package com.webservice.bookingmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ProductMicroserviceApplication {
+public class BookingMicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductMicroserviceApplication.class, args);
+		SpringApplication.run(BookingMicroserviceApplication.class, args);
 	}
 
 }
